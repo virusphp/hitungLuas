bd03b8a688c33a6640696b25980c2c1c751c1ae1
Slamet Sugandi <packercyber@gmail.com>
2017-06-08 00:48:52 +0700

    commit first androi DICODING

---
+++ app/src/main/java/id/web/dnet/hitungluas/MainActivity.java
@@ -0,0 +1,43 @@
+package id.web.dnet.hitungluas;
+
+import android.renderscript.Double2;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editPanjang, editLebar;
+    private Button btnHitung;
+    private TextView txtLuas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editPanjang = (EditText) findViewById(R.id.edit_panjang);
+        editLebar = (EditText) findViewById(R.id.edit_lebar);
+        btnHitung = (Button) findViewById(R.id.btn_hitung);
+        txtLuas = (TextView) findViewById(R.id.txt_luas);
+
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String panjang = editPanjang.getText().toString().trim();
+                String lebar = editLebar.getText().toString().trim();
+
+                double p = Double.parseDouble(panjang);
+                double l = Double.parseDouble(lebar);
+
+                double luas = p * l;
+
+                txtLuas.setText("Luas : " +luas );
+            }
+        });
+
+    }
+}
